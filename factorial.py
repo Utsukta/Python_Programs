87061b88f936f466d42bb14d558f2ac0db63cbd9
Utsukta Khatri <utsukta99kc@gmail.com>
2024-04-15 07:46:57 +0545

    Factorial

---
+++ factorial.py
@@ -0,0 +1,24 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+    print(fact)
+
+    # Write your code here
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
